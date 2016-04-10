71dc12aee7eef31e3e66ad041db8b4f9b1070e42
_nec <nec@shell8.net>
2016-04-10 15:34:07 +0200

    twits & messages work

--- web/script.js
+++ web/script.js
@@ -3,18 +3,24 @@ var socket = io('https://secure-bastion-78892.herokuapp.com/');
 socket.on('wall', function (data) {
   console.log(data);
 
+  $('.content.twitter').html('')
+
   if (data.show == 'message') {
     $('.content.message').text( data.message);
     $('body')[0].className = 'show-message'
   }
 
   if (data.show == 'twitter') {
-
-
-
-
-    $('.content.twitter').html()// = data.twitter;
-    $('body').className = 'show-twitter';
+    $('.content.twitter').html('')
+    var data= JSON.parse(data.twitter)
+    twttr.widgets.createTweet(
+      data.url.replace(/^.*status\//,''),
+      $('.content.twitter')[0],
+      {
+        cards: 'hidden'
+      }
+    )
+    $('body')[0].className = 'show-twitter';
   }
 
 });
