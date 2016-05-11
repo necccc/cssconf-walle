3a952755e1aa59e97a0bb5b48341da912b4da94f
_nec <nec@shell8.net>
2016-05-11 10:19:57 +0200

    css

--- server/ical.js
+++ server/ical.js
@@ -4,10 +4,7 @@ var ical = require('ical');
 
 var schedule = [];
 
-
-
 ical.fromURL("https://calendar.google.com/calendar/ical/cssconfbp.rocks_8qdn2o02kiasgaai9o2kffcl64%40group.calendar.google.com/public/basic.ics?ts=" + +new Date(), {}, function(err, data) {
-
 
 for (var i in data) {
   schedule.push(data[i])
